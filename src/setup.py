4dcc470540f196189b023a7dac3739d409b608eb
Midilab <contact@midilab.co>
2019-07-08 13:04:48 -0300

    fix for path changes on project directory inside setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,15 +1,12 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="opendsp",
     version="0.9.1",
     author="DSP Human interface service for headless devices",
     author_email="contact@midilab.co",
-    description="A small example package",
-    long_description=long_description,
+    description="...",
+    long_description="...",
     long_description_content_type="text/markdown",
     url="http://midilab.co/opendspd",
     packages=setuptools.find_packages(),
