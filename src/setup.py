d1418f36c361f152cbb8e2db23a8774104ebd859
midilab <contact@midilab.co>
2020-06-04 11:15:05 +0000

    update versioning for 0.10.0 version

--- src/setup.py
+++ src/setup.py
@@ -2,10 +2,9 @@ import setuptools
 
 setuptools.setup(
     name="opendsp",
-    version="0.9.1",
-    author="DSP Human interface service for headless devices",
+    version="0.10.2",
+    description="DSP interface service for headless devices",
     author_email="contact@midilab.co",
-    description="...",
     long_description="...",
     long_description_content_type="text/markdown",
     url="http://midilab.co/opendspd",
